0bade5f6756f90ef48a25888029ad5f83facc6e8
Roman Pretty <rjjpretty@gmail.com>
2026-02-21 17:10:32 +0000

    Update build process to use target directory in PostBuildEvent and enhance build.bat for file copying

--- SFR/build.bat
+++ SFR/build.bat
@@ -1,1 +1,7 @@
+SET SFD="C:\Program Files (x86)\Steam\steamapps\common\Superfighters Deluxe"
+
+copy %~1SFR.exe.config %SFD%
+copy %~1SFR.exe %SFD%
+if not exist %SFD%\SFR mkdir %SFD%\SFR
+copy %~10Harmony.dll %SFD%\SFR
 
